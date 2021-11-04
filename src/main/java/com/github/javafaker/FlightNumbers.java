298c0316af03fb7e2a52551cb5ac41f085bec26f
Wang <andreww7@fa21-cs427-017.cs.illinois.edu>
2021-11-03 19:02:18 -0500

    Initial skeleton code for generating flight numbers -  part 1 of issue 668

---
+++ src/main/java/com/github/javafaker/FlightNumbers.java
@@ -0,0 +1,13 @@
+package com.github.javafaker;
+
+public class FlightNumbers {
+    //faker object added here to emulate code in other files
+    private final Faker faker;
+
+    //Constructor added here to emulate code in other files
+    protected FlightNumbers(Faker faker) {
+        this.faker = faker;
+    }
+
+    
+}
---
+++ src/test/java/com/github/javafaker/FlightNumbersTest.java
@@ -0,0 +1,10 @@
+package com.github.javafaker;
+
+import org.junit.Test;
+
+import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
+import static org.junit.Assert.assertThat;
+
+public class FlightNumbersTest extends AbstractFakerTest {
+    
+}
