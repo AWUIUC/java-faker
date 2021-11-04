fcad9e54899e86c369ea33a197e2581af6394930
Wang <andreww7@fa21-cs427-017.cs.illinois.edu>
2021-11-03 19:19:10 -0500

    Created fake test with logger as proof of concept that testing works

--- src/test/java/com/github/javafaker/FlightNumbersTest.java
+++ src/test/java/com/github/javafaker/FlightNumbersTest.java
@@ -1,10 +1,19 @@
 package com.github.javafaker;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.github.javafaker.matchers.MatchesRegularExpression.matchesRegularExpression;
 import static org.junit.Assert.assertThat;
 
 public class FlightNumbersTest extends AbstractFakerTest {
-    
+    private static final Logger logger = LoggerFactory.getLogger(FlightNumbersTest.class);
+
+    @Test
+    public void testCode() {
+        for (int i = 0; i < 10; i++) {
+            logger.info("Testing 123 yay!!! {} to {} yayy!!! {}", -i, i, "testString");
+        }
+    }
 }
